bac5aa32778851ee31b2eba379d2a26f43f13b57
Diego Bevilacqua <dfb_98@hotmail.com>
2019-10-10 11:34:17 -0300

    ...

---
+++ SUSE/configuracion.h
@@ -0,0 +1,24 @@
+/*
+ * configuracion.h
+ *
+ *  Created on: 8 oct. 2019
+ *      Author: utnso
+ */
+
+#ifndef CONFIGURACION_H_
+#define CONFIGURACION_H_
+
+#include <stdio.h>
+#include <commons/config.h>
+
+int LISTEN_PORT;
+int METRICS_TIMER;
+int MAX_MULTIPROG;
+char** SEM_IDS;
+int* SEM_INIT;
+int* SEM_MAX;
+double ALPHA_SJF;
+
+void configurar();
+
+#endif /* CONFIGURACION_H_ */
