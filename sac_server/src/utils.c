a8cc5998b3b715876456a62e67762e89ed7dfc20
camilamamani <camilamamani38@gmail.com>
2019-11-13 00:48:36 -0300

    agrego dos funciones para el path

--- sac_server/src/utils.c
+++ sac_server/src/utils.c
@@ -1,5 +1,13 @@
 #include "utils.h"
 
+uint64_t get_current_time()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	return (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+}
+
 size_t get_filesize(char *filename)
 {
 	FILE* fd = fopen(filename, "r");
@@ -33,4 +41,22 @@ void reverse_string_vector(char ** strings)
 	}
 }
 
+//desc: obtengo el nombre del ultimo file del path
+char* get_last_filename_from_path(char *path)
+{
+	char **path_names = string_split(path, "/");
+	int path_elems = get_size_filenames(path_names);
 
+	return path_names[path_elems-1];
+}
+
+//desc: obtengo de /home/casa/lala.txt a /home/casa
+char* get_lastfile_previous_path(char *path)
+{
+	char **pathnames = string_split(path, "/");
+	int path_elems = get_size_filenames(pathnames);
+	int tam_last_filename = strlen( pathnames[path_elems - 1] );
+
+	return string_substring(path, 0, strlen(path) - tam_last_filename);
+
+}
--- sac_server/src/utils.h
+++ sac_server/src/utils.h
@@ -3,9 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/string.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <string.h>
 
+uint64_t get_current_time();
 size_t get_filesize(char *filename);
 void reverse_string_vector(char **strings);
 int get_size_filenames(char **names);
+char* get_last_filename_from_path(char *path);
+char* get_lastfile_previous_path(char *path);
 
 #endif /* UTILS_H_ */
