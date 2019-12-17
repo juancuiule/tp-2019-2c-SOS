5d93f731c77125571e833b9ffe8a27e9d253d387
Juan Cuiule <juanchicuiu@gmail.com>
2019-12-17 15:03:14 -0300

    change gitignore

--- prueba/Debug/src/subdir.mk
+++ prueba/Debug/src/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/Documentos/tp-ordenado/libmuse" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/utnso/Documentos/tp-2019-2c-SOS/libmuse" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
